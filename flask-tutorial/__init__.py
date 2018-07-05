8b0f7be5c6bca52959109940d7ead8d08869b7f3
Maksim Rodin <rodin.maxim93@gmail.com>
2018-07-05 19:10:26 +0200

    Add main page and initial static files

--- flask-tutorial/__init__.py
+++ flask-tutorial/__init__.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    return "Hi there, hello world from Flask"
+    return render_template('main.html')
 
 
 if __name__ == '__main__':
