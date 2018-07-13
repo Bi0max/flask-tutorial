6993f5b926de37a9f5ab0de0c678ab351f0abf72
Maksim Rodin <rodin.maxim93@gmail.com>
2018-07-13 18:46:25 +0200

    Add error handling

--- flask_tutorial/app.py
+++ flask_tutorial/app.py
@@ -18,5 +18,19 @@ def dashboard():
     return render_template('dashboard.html', TOPIC_DICT=TOPIC_DICT)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.route('/slashboard/')
+def slashboard():
+    try:
+        raise ValueError("Show error")
+        return render_template('dashboard.html', TOPIC_DICT=TOPIC_DICT)
+    except Exception as e:
+        return render_template('500.html', error=e)
+
+
 if __name__ == '__main__':
     app.run()
