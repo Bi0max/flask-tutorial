db1e8ccca31326cc12e34b7cf3d6639cabd42d08
Maksim Rodin <rodin.maxim93@gmail.com>
2018-07-18 19:49:52 +0200

    Add charting tutorial with pygal

--- flask_tutorial/app.py
+++ flask_tutorial/app.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime, timedelta
 from functools import wraps
 
+import pygal
 from flask import Flask, render_template, flash, request, url_for, redirect, session, g, make_response, send_file, \
     send_from_directory, jsonify
 from passlib.hash import sha256_crypt
@@ -301,6 +302,22 @@ def background_process():
     except Exception as e:
         return str(e)
 
+
+@app.route('/pygal_example/')
+def pygal_example():
+    try:
+
+        graph = pygal.Line()
+        graph.title = '% Change Coolness of programming languages over time.'
+        graph.x_labels = ['2011', '2012', '2013', '2014', '2015', '2016']
+        graph.add('Python', [15, 31, 89, 200, 356, 900])
+        graph.add('Java', [15, 45, 76, 80, 91, 95])
+        graph.add('C++', [5, 51, 54, 102, 150, 201])
+        graph.add('All others combined!', [5, 15, 21, 55, 92, 105])
+        graph_data = graph.render_data_uri()
+        return render_template('graphing.html', graph_data=graph_data)
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run()
